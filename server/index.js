be52aeb449f0d9fc59bd917b0025a264d9218cda
NikolayBorzunov <forest3694@ya.ru>
2021-06-25 13:05:22 +0300

    login/signup server functionality

--- server/index.js
+++ server/index.js
@@ -5,8 +5,12 @@ const app = express();
 
 connectDB();
 
+app.use(express.json({ extended: false}));
+
 app.get("/", (req, res) => res.send("api running"));
 
+
+app.use('/auth', require('./routes/auth'))
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
