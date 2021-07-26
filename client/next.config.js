9fe7f1ef24d7bfb0df5b944a6070fa1ec929c069
NikolayBorzunov <forest3694@ya.ru>
2021-07-26 12:16:31 +0300

    get image from google cloud to frontend

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['source.unsplash.com', 'blob:http://localhost:3000', '']
+    domains: ['source.unsplash.com', 'blob:http://localhost:3000', '', 'storage.googleapis.com']
   },
   env: {
     NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID
