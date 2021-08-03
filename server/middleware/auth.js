3c5233bbbe4982b8cf3a92044f7f4fe8a212a698
NikolayBorzunov <forest3694@ya.ru>
2021-08-03 12:47:27 +0300

    refactor: changed auth token storage from localStorage to cookies

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,21 +1,19 @@
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
+  const token = req.header("x-auth-token");
 
-    const token = req.header('x-auth-token');
+  if (!token) {
+    return res.status(401).send({ msg: "No token, authorization denied" });
+  }
 
-    if(!token) {
-        return res.status(401).json({msg: 'No token, authorization denied' });
-    }
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecretKey"));
 
-    try{
-
-        const decoded = jwt.verify(token, config.get('jwtSecretKey'));
-        
-        req.user = decoded.user;
-        next();
-    } catch(err) {
-        res.status(401).json({ msg: 'Token is not valid'});
-    }
-}
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).send({ msg: "Token is not valid" });
+  }
+};
