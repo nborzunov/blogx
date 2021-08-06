e71e7839d49ecff0cac7af19fe0a0d6ac8153356
NikolayBorzunov <forest3694@ya.ru>
2021-08-06 09:31:45 +0300

    feat: logout

--- client/store/types.js
+++ client/store/types.js
@@ -1,6 +1,8 @@
 export const GET_USER = 'GET_USER';
 export const LOGIN_USER = 'LOGIN_USER';
+export const LOGOUT = 'LOGOUT';
 
 export const SET_ERROR = 'SET_ERROR';
 
 export const SET_PROFILE = 'SET_PROFILE';
+export const CLEAR_PROFILE = 'CLEAR_PROFILE';
--- client/utils/cookieService.js
+++ client/utils/cookieService.js
@@ -1,52 +1,54 @@
-class CookieService {
-    getCookie(name) {
-        var matches = document.cookie.match(
-            new RegExp(
-                '(?:^|; )' +
-                    name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') +
-                    '=([^;]*)'
-            )
-        );
-        return matches ? decodeURIComponent(matches[1]) : undefined;
-    }
-
-    setCookie(name, value, props) {
-        props = props || {};
-
-        var exp = props.expires;
-
-        if (typeof exp == 'number' && exp) {
-            var d = new Date();
-
-            d.setTime(d.getTime() + exp * 1000);
-
-            exp = props.expires = d;
-        }
-
-        if (exp && exp.toUTCString) {
-            props.expires = exp.toUTCString();
-        }
-
-        value = encodeURIComponent(value);
-
-        var updatedCookie = name + '=' + value;
-
-        for (var propName in props) {
-            updatedCookie += '; ' + propName;
-
-            var propValue = props[propName];
-
-            if (propValue !== true) {
-                updatedCookie += '=' + propValue;
-            }
-        }
-
-        document.cookie = updatedCookie;
-    }
-
-    deleteCookie(name) {
-        setCookie(name, null, { expires: -1 });
-    }
+function getCookie(name) {
+    var matches = document.cookie.match(
+        new RegExp(
+            '(?:^|; )' +
+                name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') +
+                '=([^;]*)'
+        )
+    );
+    return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
-export default new CookieService();
+function setCookie(name, value, props) {
+    props = props || {};
+
+    var exp = props.expires;
+
+    if (typeof exp == 'number' && exp) {
+        var d = new Date();
+
+        d.setTime(d.getTime() + exp * 1000);
+
+        exp = props.expires = d;
+    }
+
+    if (exp && exp.toUTCString) {
+        props.expires = exp.toUTCString();
+    }
+
+    value = encodeURIComponent(value);
+
+    var updatedCookie = name + '=' + value;
+
+    for (var propName in props) {
+        updatedCookie += '; ' + propName;
+
+        var propValue = props[propName];
+
+        if (propValue !== true) {
+            updatedCookie += '=' + propValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+
+function deleteCookie(name) {
+    setCookie(name, null, { expires: -1, path: '/' });
+}
+
+export default {
+    getCookie,
+    setCookie,
+    deleteCookie,
+};
